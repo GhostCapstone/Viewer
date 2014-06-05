dd355b349fe78ac290eee7372cd7f8befbd01743
Ngoc Do <ngocmdo@gmail.com>
2014-06-05 08:47:38 -0700

    remove borders from 2canvas

--- scripts/CGA_GraphicsEngine.js
+++ scripts/CGA_GraphicsEngine.js
@@ -46,7 +46,6 @@ var CGA_GraphicsEngine = function(config)
         leapCanvas.style.left = this.windowLeft + 'px';
         leapCanvas.style.top = this.windowTop + 'px';
         leapCanvas.style.zIndex = 2;
-        leapCanvas.style.border = "1px solid red";
     
     var uiContainer = document.getElementById('ui_container');
         uiContainer.style.left = this.windowLeft + 'px';
